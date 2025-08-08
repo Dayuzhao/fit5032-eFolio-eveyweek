0fd47a2f6dc0fabcf17e56fbbc0477b9eaab66e3
dzha0064 <dzha0064@student.monash.edu>
2025-08-08 21:04:24 +1000

    first commit

---
+++ src/components/MyName.vue
@@ -0,0 +1,17 @@
+<!-- src/components/MyName.vue -->
+<template>
+  <div class="my-name">
+    <h2>Hello, my name is Dayu Zhao!</h2>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+  color: #42b983;
+  font-weight: bold;
+}
+</style>
