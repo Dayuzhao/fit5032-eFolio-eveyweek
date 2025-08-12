07b5a64d3b05e626af3f461115946b0f8abfa932
dzha0064 <dzha0064@student.monash.edu>
2025-08-12 17:29:24 +1000

    w3 first push

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
@@ -18,6 +19,7 @@ import HelloWorld from './components/HelloWorld.vue'
   </header>
 
   <RouterView />
+  <Form />
 </template>
 
 <style scoped>
